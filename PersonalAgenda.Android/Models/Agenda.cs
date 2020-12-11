bda4e74a3766df486f7fadb75df64861a73c4337
Ignat Cristian <cristianignat98@gmail.com>
2020-12-11 19:45:47 +0200

    Add project files.

---
+++ PersonalAgenda.Android/Models/Agenda.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PersonalAgenda.Models
+{
+    public class Agenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ PersonalAgenda/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PersonalAgenda.Data;
+using System.IO;
+
+namespace PersonalAgenda
+{
+    public partial class App : Application
+    {
+        static AgendaDatabase Database;
+
+        public static AgendaDatabase Database
+        {
+            get
+            {
+                if (Database == null)
+                {
+                    Database = new
+                        AgendaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+LocalApplicationData), "Notes.db3"));
+                }
+                return Database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new AgendaPage());
+        }
+
+                    MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
